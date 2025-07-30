39072cf3d9a8ceb30886ee9d66257047d13c7110
Ebru Gülçiçek <52013451+gulcicekebru@users.noreply.github.com>
2025-07-30 14:14:23 +0300

    reorganized project struture and reloaded solution

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.API.Models;
+
+
+namespace TaskFlow.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
