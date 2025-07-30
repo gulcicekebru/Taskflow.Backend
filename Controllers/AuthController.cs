2a95daba6278f8bf5d88ff13a35cf2915bff6181
Ebru Gülçiçek <52013451+gulcicekebru@users.noreply.github.com>
2025-07-30 16:54:11 +0300

    register and login and password hashing added.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.API.Data;
+using TaskFlow.API.DTOs;
+using TaskFlow.API.Models;
+using TaskFlow.API.Services;
+
+
+namespace TaskFlow.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public AuthController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto request)
+        {
+            if(await _context.Users.AnyAsync(u => u.Username == request.Username)) 
+                return BadRequest("Username already exist.");
+
+            AuthHelper.CreatePasswordHash(request.Password, out byte[] hash, out byte[] salt);
+
+            var user = new User
+            {
+                Username = request.Username,
+                PasswordHash = hash,
+                PasswordSalt = salt
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("User registered successfully");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+
+            if (user == null)
+                return BadRequest("User no found");
+
+            if (!AuthHelper.VerifyPassword(request.Password, user.PasswordHash, user.PasswordSalt))
+                return BadRequest("Invalid password.");
+
+            return Ok("Login successful (Token will be added)");
+        }
+    }
+}
