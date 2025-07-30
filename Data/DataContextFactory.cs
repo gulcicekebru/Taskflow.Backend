cdcc400273e9bc885fab6a73fd64ddcc8eab7754
Ebru Gülçiçek <52013451+gulcicekebru@users.noreply.github.com>
2025-07-30 14:40:39 +0300

    fix: added design-time DbContext factory for EF migrations

---
+++ Data/DataContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using TaskFlow.API.Data;
+
+
+namespace TaskFlow.API.Data
+{
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args) 
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TaskFlowDb;Trusted_Connection=True;");
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
