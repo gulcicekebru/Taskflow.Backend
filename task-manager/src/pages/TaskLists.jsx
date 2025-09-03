fd8efdeb6f2fbbb13e77449e2b00de420c89819b
Ebru Gülçiçek <52013451+gulcicekebru@users.noreply.github.com>
2025-09-03 21:04:10 +0300

    Unnecessary codes deleted

--- task-manager/src/pages/TaskLists.jsx
+++ task-manager/src/pages/TaskLists.jsx
@@ -43,9 +43,6 @@ const TaskList = () => {
 
     const handleToggleTaskState = (id) => {
         const taskToUpdate = tasks.find(t => t.id === id);
-        if (taskToUpdate.isCompleted) {
-            return;
-        }
 
         TaskService.completeTask(id).then(res => {
             setTasks(tasks.map(task => task.id === id ? { ...task, isCompleted: !task.isCompleted} : task));
